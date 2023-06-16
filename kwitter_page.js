9ec2e9aeec0211de8a38a685fde12a53c3c2fca4
Prakriti123456 <93470873+Prakriti123456@users.noreply.github.com>
2023-06-16 14:04:27 +0530

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,12 +1,12 @@
 //YOUR FIREBASE LINKS
 var firebaseConfig = {
-      apiKey: "AIzaSyA-z_Z7awqezTIZBc3QYn0pLjujhrNiZR0",
-      authDomain: "lets-chat-4c3c0.firebaseapp.com",
-      databaseURL: "https://lets-chat-4c3c0-default-rtdb.firebaseio.com",
-      projectId: "lets-chat-4c3c0",
-      storageBucket: "lets-chat-4c3c0.appspot.com",
-      messagingSenderId: "855798776097",
-      appId: "1:855798776097:web:739b3c46424f757b72fe60"
+  apiKey: "AIzaSyA-z_Z7awqezTIZBc3QYn0pLjujhrNiZR0",
+  authDomain: "lets-chat-4c3c0.firebaseapp.com",
+  databaseURL: "https://lets-chat-4c3c0-default-rtdb.firebaseio.com",
+  projectId: "lets-chat-4c3c0",
+  storageBucket: "lets-chat-4c3c0.appspot.com",
+  messagingSenderId: "855798776097",
+  appId: "1:855798776097:web:739b3c46424f757b72fe60"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
